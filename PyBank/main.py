8eb53726a855b3b42ba02a3ac823b5150ea51f19
KyleDamian <khaddock@live.com>
2018-09-30 13:25:21 -0500

    Added challenge folders and main files

---
+++ PyBank/main.py
@@ -0,0 +1,6 @@
+import os
+import csv
+
+# Path to collect data from the Resources folder
+csvfile = os.path.join("..", "..", "03-Python" "Homework", "Instructions", "PyBank", "Resources", "budget_dat.csv")
+
