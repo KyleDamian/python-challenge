4fc7f67a2fcaa25e2c8fea291445a5ce488b8c30
KyleDamian <khaddock@live.com>
2018-09-30 18:49:53 -0500

    Saving starting code to PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,5 +2,21 @@ import os
 import csv
 
 # Path to collect data from the Resources folder
-csvfile = os.path.join("..", "..", "03-Python" "Homework", "Instructions", "PyBank", "Resources", "budget_dat.csv")
+csvfile = os.path.join("budget_data.csv")
+
+
+print("FINANCIAL ANALYSIS")
+print("----------------------------------")
+
+def averageChange(budgetData)
+    
+  
+
+
+
+with open(csvfile, 'r') as csvfile:
+
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    header = next(csvreader)
 
